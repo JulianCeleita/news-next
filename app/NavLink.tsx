69e3b2bcea716c2e9e5bcd002b5fb7c93d9efeb5
Julian Celeita <andresceleita@yahoo.es>
2023-03-19 09:50:17 -0500

    added isActive and elements navLink to the navBar

---
+++ app/NavLink.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+type Props = {
+  category: string;
+  isActive: boolean;
+};
+
+function NavLink({ category, isActive }: Props) {
+  return <Link href={`/news/${category}`} 
+  className={`navLink ${isActive && 
+    "underline decoration-red-500 underline-offset-4 fonr-bold text-lg"}`}>
+        {category}
+        </Link>;
+}
+export default NavLink;
--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -1,6 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { categories } from "../constants";
+import NavLink from "./NavLink";
+
 function NavLinks() {
+  const pathname = usePathname();
+  const isActive = (path: string) => {
+    return pathname?.split("/").pop() === path;
+  };
+
   return (
-    <div>NavLinks</div>
-  )
+    <nav>
+      {categories.map((category) => (
+        <NavLink
+          key={category}
+          category={category}
+          isActive={isActive(category)}
+        />
+      ))}
+    </nav>
+  );
 }
-export default NavLinks
+export default NavLinks;
