cf198febf63bc23bd46ce8065a753a8db9e0f570
Julian Celeita <andresceleita@yahoo.es>
2023-03-29 12:27:42 -0500

    test vercel changes

--- app/ReadMoreButton.tsx
+++ app/ReadMoreButton.tsx
@@ -18,7 +18,7 @@ function ReadMoreButton({article}: Props) {
     onClick={handleClick}
     className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500"
   >
-    Read More
+    Read Article
   </button>
 }
 export default ReadMoreButton
